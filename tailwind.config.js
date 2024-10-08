dfc831c68036f759658fad0d1215690c1d846dc8
keaguirre <ke.aguirre@duocuc.cl>
2024-10-08 02:36:00 -0300

    First upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./src/**/*.{astro,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Bebas Neue', 'system-ui', 'sans-serif'],
+      },
+      colors: {
+        'custom-blue': '#3871c1',
+        'custom-dark-blue': '#00275b',
+      },
+    },
+  },
+  plugins: [],
+}
