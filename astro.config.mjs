7ec18e94b0a7f04d4e44be4829c0da68a6a4ad6a
keaguirre <ke.aguirre@duocuc.cl>
2024-10-14 01:04:37 -0300

    fix base route

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,9 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   output: 'static', // define el modo de renderizado {static, hydrid, server}
   integrations: [tailwind()],
+  site: 'https://space-sync-dh.github.io/Space-Sync-Front/', // Define la URL base de tu sitio
+  base: 'Space-Sync-Front', // Define la URL base de tu sitio
   build: {
     outDir: './dist', // Define el directorio de salida
   },
-  site: 'https://space-sync-dh.github.io/Space-Sync-Front', // Define la URL base de tu sitio
-  base: 'Space-Sync-Front', // Define la URL base de tu sitio
 });
