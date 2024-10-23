3ccebca6a1a17e6c464997f1cc88baa4f63d3a6c
PaulaNarvaez <paul.cortes@duocuc.cl>
2024-10-23 03:22:49 -0300

    Page Reserva y Dashboard

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react'; // Importa la integración de React
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  output: 'static', // define el modo de renderizado {static, hydrid, server}
+  integrations: [
+    tailwind(), // Mantén la integración de Tailwind
+    react(),    // Agrega la integración de React
+  ],
+  output: 'static', // define el modo de renderizado {static, hybrid, server}
   build: {
     outDir: './dist', // Define el directorio de salida
   },
